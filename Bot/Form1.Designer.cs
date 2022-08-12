7cda6e2cefa1aa6867ff444f31cac4cdcb49885b
Kolya <kolya.ua921@gmail.com>
2022-08-12 16:55:32 +0300

    Открывается Мозила. Переход на гугл. Поиск в гугле со строки!

--- Bot/Form1.Designer.cs
+++ Bot/Form1.Designer.cs
@@ -28,13 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_google = new System.Windows.Forms.Button();
+            this.button_enter = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_google
+            // 
+            this.button_google.Location = new System.Drawing.Point(150, 135);
+            this.button_google.Name = "button_google";
+            this.button_google.Size = new System.Drawing.Size(146, 136);
+            this.button_google.TabIndex = 0;
+            this.button_google.Text = "GOOGLE";
+            this.button_google.UseVisualStyleBackColor = true;
+            this.button_google.Click += new System.EventHandler(this.button_google_Click);
+            // 
+            // button_enter
+            // 
+            this.button_enter.Location = new System.Drawing.Point(320, 135);
+            this.button_enter.Name = "button_enter";
+            this.button_enter.Size = new System.Drawing.Size(150, 136);
+            this.button_enter.TabIndex = 1;
+            this.button_enter.Text = "ENTER";
+            this.button_enter.UseVisualStyleBackColor = true;
+            this.button_enter.Click += new System.EventHandler(this.button_enter_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(320, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_enter);
+            this.Controls.Add(this.button_google);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_google;
+        private System.Windows.Forms.Button button_enter;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Bot/Form1.cs
+++ Bot/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Keys = OpenQA.Selenium.Keys;
 
 namespace Bot
 {
+   
     public partial class Form1 : Form
     {
+        public FirefoxDriver firefox;
         public Form1()
         {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = true;
+        }
+
+        private void button_google_Click(object sender, EventArgs e)
+        {
+            firefox.Navigate().GoToUrl("https://www.google.com/");
+        }
+
+        private void button_enter_Click(object sender, EventArgs e)
+        {
+            string textPoisk = textBox1.Text;
+            firefox.FindElement(By.XPath("//input[@class = 'gLFyf gsfi']")).SendKeys(textPoisk);
+            firefox.FindElement(By.XPath("//input[@class = 'gLFyf gsfi']")).SendKeys(Keys.Enter);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (firefox != null) firefox.Quit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitFirefox();
+        }
+
+        public void InitFirefox()
+        {
+            FirefoxDriverService servise = FirefoxDriverService.CreateDefaultService();
+            servise.HideCommandPromptWindow = true;
+
+            FirefoxOptions options = new FirefoxOptions();
+            firefox = new FirefoxDriver(servise, options);
         }
     }
 }
